793259e2f2d8ee37e2344419b7895123f218e101
TacoKomine <71116842+TacoKomine@users.noreply.github.com>
2020-11-08 11:42:54 +0900

    ファイル一式
    提出用ファイル一式。html, css, img

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// jsを記述する際はここに記載していく
+$(function() {
+    var topBtn = $('.to-top');
+    //ボタンを非表示にする
+    topBtn.hide();
+    //スクロールしてページトップから100に達したらボタンを表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+　　　　　　　//フェードインで表示
+            topBtn.fadeIn();
+        } else {
+　　　　　　　//フェードアウトで非表示
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップへ戻る
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
